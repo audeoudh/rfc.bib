dcd8f5c121b94749ac2ac61a04afc27b884372a2
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2018-12-17 21:10:05 +0100

    Use argparse to parse the CLI arguments
    Two methods to call the script:
    - import it from another one.  Then, use the `main` method.
    - launch the script itself.  The last if is run, arguments parsed, and `main` called.

--- rfc2bib.py
+++ rfc2bib.py
@@ -3,6 +3,7 @@ URL_RFC_XML = 'http://www.rfc-editor.org/in-notes/rfc-index.xml'
 
 from lxml import etree
 import requests
+import argparse
 
 BIB_ENTRY_FORMAT = """\
 @techreport{%(key)s,
@@ -28,21 +29,27 @@ def normalize_authors(authors):
     authors_reversed = map(lambda a: a.split()[-1] + ', ' + ' '.join(a.split()[0:-1]), authors)
     return ' and '.join(authors_reversed)
 
-index = etree.fromstring(requests.get(URL_RFC_XML).content)
-rfcs = index.findall(tag_prefix('rfc-entry'))
+def main():
+    index = etree.fromstring(requests.get(URL_RFC_XML).content)
+    rfcs = index.findall(tag_prefix('rfc-entry'))
 
-#for r in rfcs[0:30]:
-for r in rfcs:
-    d = {}
-    d['key'] = r.find(tag_prefix('doc-id')).text
-    d['number'] = int(d['key'][3:])
-    d['title'] = r.find(tag_prefix('title')).text
-    _a = []
-    for a in r.findall(tag_prefix('author')):
-        _a += [a.find(tag_prefix('name')).text]
-    d['author'] = normalize_authors(_a)
-    d['year'] = r.find(tag_prefix('date')).find(tag_prefix('year')).text
-    d['month'] = r.find(tag_prefix('date')).find(tag_prefix('month')).text[:3].lower()
-    d['url'] = 'http://tools.ietf.org/rfc/%s.txt' % d['key'].lower()
-    #d['obsoletes'] = ','.join([o.find('doc-id').text for o in r.findall(tag_prefix('obsoletes'))])
-    print(BIB_ENTRY_FORMAT % d)
+    for r in rfcs:
+        d = {}
+        d['key'] = r.find(tag_prefix('doc-id')).text
+        d['number'] = int(d['key'][3:])
+        d['title'] = r.find(tag_prefix('title')).text
+        _a = []
+        for a in r.findall(tag_prefix('author')):
+            _a += [a.find(tag_prefix('name')).text]
+        d['author'] = normalize_authors(_a)
+        d['year'] = r.find(tag_prefix('date')).find(tag_prefix('year')).text
+        d['month'] = r.find(tag_prefix('date')).find(tag_prefix('month')).text[:3].lower()
+        d['url'] = 'http://tools.ietf.org/rfc/%s.txt' % d['key'].lower()
+        #d['obsoletes'] = ','.join([o.find('doc-id').text for o in r.findall(tag_prefix('obsoletes'))])
+        print(BIB_ENTRY_FORMAT % d)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    main()
